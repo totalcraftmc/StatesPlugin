078f432030be9141d06194a56409277280dde8c2
degan <ivanshostik@yandex.ru>
2024-06-10 21:56:22 +0300

    Initial commit

---
+++ src/main/java/ru/totalcraftmc/statesplugin/StatesPlugin.java
@@ -0,0 +1,17 @@
+package ru.totalcraftmc.statesplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StatesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
