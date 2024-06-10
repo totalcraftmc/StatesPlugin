aff873780e6dc38b9ff4ee3bfacc6ba9e5866a89
degan <ivanshostik@yandex.ru>
2024-06-10 23:36:45 +0300

    Initial commit

--- src/main/java/ru/totalcraftmc/statesplugin/StatesPlugin.java
+++ src/main/java/ru/totalcraftmc/statesplugin/StatesPlugin.java
@@ -1,12 +1,20 @@
 package ru.totalcraftmc.statesplugin;
 
+import jakarta.persistence.EntityManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.Collections;
 
 public final class StatesPlugin extends JavaPlugin {
 
+    public static StatesPlugin instance;
+    public static EntityManager entityManager = new HibernatePersistenceProvider().createEntityManagerFactory("persistence", Collections.emptyMap()).createEntityManager();
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
